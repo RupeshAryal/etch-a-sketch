82dc3f8369a90ff9af8bb705a1866d0ec8d4d3a9
RupeshAryal <aryalrupesh2074@gmail.com>
2022-02-28 18:32:22 +0545

    somewhat, finishedn need a lot of working, decided to move forward learning othher conecept

--- script.js
+++ script.js
@@ -3,34 +3,94 @@ const toggleGrid = document.querySelector('.grid');
 const clear = document.querySelector('.clear');
 const eraser = document.querySelector('.eraser');
 const color = document.querySelector('.color');
+const sixteen = document.querySelector('.sixteen');
+const thiryTwo = document.querySelector('.thirty-two');
 square.setAttribute('style', 'display:flex; flex-wrap: wrap;');
+sixteen.checked = true;
+let innerdivs;
+createDivs(16);
+
+
+sixteen.addEventListener('click', ()=>{
+ 
+  innerdivs.forEach((div)=>{
+  div.classList.remove('squaresquare')})
+  square.innerHTML = '';
+  if (thiryTwo.checked) {
+    thiryTwo.checked = false;
+  }
+  //createDivs(16);
+  console.log(innerdivs)
+  });
+
+thiryTwo.addEventListener('click', ()=>{
+  innerdivs.forEach((div)=>{
+    div.classList.remove('squaresquare')})
+  square.innerHTML = '';
+
+  if (sixteen.checked) {
+    sixteen.checked = false;  
+  }
+  colors();
+
+  //createDivs(32);
+  console.log(innerdivs)
+});
+
+
  
 
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {   
-      const create = document.createElement('div');
-      create.setAttribute('style', 'width:6.25%; height:6.25%;' );
-      create.setAttribute('class', 'squareclass')
-      square.appendChild(create);
-    }    
+function createDivs(gridLength) {
+  for (let i = 0; i < gridLength; i++) {
+    for (let j = 0; j < gridLength; j++) {   
+      ;
+      if (gridLength >16) {
+        const create = document.createElement('div')
+        create.setAttribute('style', 'height:3.125%; width:3.125%;');
+        create.setAttribute('class', 'squareclass')
+        square.appendChild(create);
+      }
+      else{
+        const create = document.createElement('div')
+        create.setAttribute('style', 'height:6.25%; width:6.25%;');
+        create.setAttribute('class', 'squareclass')
+        square.appendChild(create);
+      }  
+    }  
+   
    }
-  
-const innerdivs=document.querySelectorAll('.squareclass');
+   innerdivs = document.querySelectorAll('.squareclass')  
+  }
 
-innerdivs.forEach((div)=>{
-  div.addEventListener("mouseover", ()=>{
-    if (eraser.active == true) {
-      div.style.backgroundColor = 'white';
-    }
-    else if (color.active == true) {
-      div.backgroundColor = 'black';
-    }
-    else{
-      div.style.backgroundColor = 'black';
-    }    
-  });
-  })
+  console.log(innerdivs)
+   function changeColor(e) {
+  if (eraser.classList.contains('active')) {
+    e.target.style.backgroundColor = 'white';
+    
+  }
+  else {e.target.style.backgroundColor = 'black';}
+}
+
+function colors()
+ {
+   innerdivs.forEach((div)=>{
+  div.addEventListener('mouseover', changeColor, false);
+})
+  }
+
+// innerdivs.forEach((div)=>{
+//   div.addEventListener("mouseover", ()=>{
+//     if (eraser.active == true) {
+//       div.style.backgroundColor = 'white';
+//     }
+//     else if (color.active == true) {
+//       div.backgroundColor = 'black';
+//     }
+//     else{
+//       div.style.backgroundColor = 'black';
+//     }    
+//   });
+//   })
 
 
 
@@ -56,27 +116,25 @@ clear.addEventListener('click', ()=>{
 })
 
 eraser.addEventListener('click', ()=>{
-  innerdivs.forEach((div)=>{
-    
-      div.addEventListener("mouseover", ()=>{
-        div.style.backgroundColor = 'white';
+  eraser.classList.add('active');
+  color.classList.remove('active');
+  console.log(color.className);
+  console.log(eraser.className);
     });
-    
-    })
-})
+
 
 color.addEventListener('click', ()=>{
-  //eraser.removeEventListener('click', false);
-  
-  innerdivs.forEach((div)=>{
-    //div.removeEventListener('mouseover',()=>{
+ color.classList.add('active');
+ eraser.classList.remove('active');
+ console.log(color.className);
+ console.log(eraser.className);
 
-    //})
-    div.addEventListener("mouseover", ()=>{
-        div.style.backgroundColor = 'black';
     });
-    })
-})
+
+
+
+  
+
 
 
 
